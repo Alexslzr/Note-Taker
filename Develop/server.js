f400a040eacb7f24f095e90e341e7e3e48872cd9
Alejandro Salazar <alejandroslzr17@gmail.com>
2023-05-31 14:18:57 -0700

    added the uuid file to create random id to each of the notes, added the post method in order to post a new note

--- Develop/server.js
+++ Develop/server.js
@@ -2,24 +2,77 @@ const express = require('express')
 const PORT = 3001;
 const app = express();
 const path = require('path');
+const data = require('./db/db.json')
+const uuid = require('./helper/uuid')
+const fs = require('fs')
 
 
 app.use(express.json());
-
-
-
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 // GET Route for homepage
-/*app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/assets/index.html'))
-);*/
+app.get('/api/notes', (req, res) =>
+  res.json(data)
+);
 
 // GET Route for feedback page
 app.get('/notes', (req, res) =>
-
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.post('/api/notes', (req, res)=>{
+    console.info(`${req.method} request received to add a review`);
+  
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        note_id: uuid(),
+      };
+  
+      // Obtain existing reviews
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          // Convert string into JSON object
+          const parsedNotes = JSON.parse(data);
+  
+          // Add a new review
+          parsedNotes.push(newNote);
+  
+          // Write updated reviews back to the file
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedNote, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated Notes!')
+          );
+        }
+      });
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in posting Note');
+    }
+
+
+
+})
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)

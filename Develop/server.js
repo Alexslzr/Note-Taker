e14ef02a9752f00f7bc3073991e23463f8b4fb8a
Alejandro Salazar <alejandroslzr17@gmail.com>
2023-05-31 14:42:40 -0700

    added the functionality of seeing each note specific note with get method, changed note_id var to id so it matches the frontend

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const PORT = 3001;
 const app = express();
 const path = require('path');
-const data = require('./db/db.json')
+const notes = require('./db/db.json')
 const uuid = require('./helper/uuid')
 const fs = require('fs')
 
@@ -12,12 +12,30 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) =>
-  res.json(data)
+  res.json(notes)
 );
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes/:id', (req, res) => {
+  if (req.params.id) {
+    console.info(`${req.method} request received to get a specific note`);
+    const noteId = req.params.id;
+    for (let i = 0; i < notes.length; i++) {
+      const currentNote = notes[i];
+      if (currentNote.id === noteId) {
+        res.status(200).json(currentNote);
+        return;
+      }
+    }
+    res.status(404).send('Note not found');
+  } else {
+    res.status(400).send('Note ID not provided');
+  }
+});
+
 
 app.post('/api/notes', (req, res)=>{
     console.info(`${req.method} request received`);
@@ -26,13 +44,13 @@ app.post('/api/notes', (req, res)=>{
       const newNote = {
         title,
         text,
-        note_id: uuid(),
+        id: uuid(),
       };
-      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      fs.readFile('./db/db.json', 'utf8', (err, notes) => {
         if (err) {
           console.error(err);
         } else {
-          const parsedNotes = JSON.parse(data);
+          const parsedNotes = JSON.parse(notes);
           parsedNotes.push(newNote);
           fs.writeFile('./db/db.json',JSON.stringify(parsedNotes, null, 4),
             (writeErr) =>

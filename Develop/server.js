add6cffd7c530303242b7b6052fbfd6a3ed2a62c
Alejandro Salazar <alejandroslzr17@gmail.com>
2023-06-06 12:12:27 -0600

    added gitignore,added the res.json(notes) so it get updated directly the notes in the get and the post

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const path = require('path');
-const notes = require('./db/db.json')
+let notes = require('./db/db.json')
 const uuid = require('./helper/uuid')
 const fs = require('fs')
 
@@ -10,6 +10,7 @@ const fs = require('fs')
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+
 
 app.get('/api/notes', (req, res) =>
   res.json(notes)
@@ -46,38 +47,34 @@ app.post('/api/notes', (req, res)=>{
         text,
         id: uuid(),
       };
-      fs.readFile('./db/db.json', 'utf8', (err, notes) => {
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
           console.error(err);
         } else {
-          const parsedNotes = JSON.parse(notes);
+          const parsedNotes = JSON.parse(data);
           parsedNotes.push(newNote);
+          notes = parsedNotes
+          console.log(parsedNotes)
           fs.writeFile('./db/db.json',JSON.stringify(parsedNotes, null, 4),
             (writeErr) =>
               writeErr
                 ? console.error(writeErr)
-                : console.info('Successfully updated Notes!')
+                : console.info('success')
           );
+          res.json(notes)
         }
       });
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-       console.log(response);
-      res.status(201).json(response);
     } else {
       res.status(500).json('Error in posting Note');
     }
 })
 
 app.delete('/api/notes/:id', (req,res)=>{
-
- fs.readFile('./db/db.json', 'utf8', (err, notes) => {
+ fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
           console.error(err);
         } else {
-          const parsedNotes = JSON.parse(notes);
+          const parsedNotes = JSON.parse(data);
           let deletedNote = req.params.id;
           let x;
           for(let i=0; i<parsedNotes.length;i++){
@@ -86,16 +83,21 @@ app.delete('/api/notes/:id', (req,res)=>{
             }
           }
           parsedNotes.splice(x,1);
+          notes=parsedNotes
           fs.writeFile('./db/db.json',JSON.stringify(parsedNotes, null, 4),
             (writeErr) =>
               writeErr
                 ? console.error(writeErr)
                 : console.info('Successfully updated Notes!')
           );
+          res.json(notes)
         }
       });
+})
 
 
+app.get('*', (req,res)=>{
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 app.listen(PORT, () =>
